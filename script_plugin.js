e530912c347e3690eba2ff27444aa56cd743a921
Phu Nguyen <pisun2@gmail.com>
2014-01-30 22:00:29 +0700

    Create script_plugin.js

---
+++ script_plugin.js
@@ -0,0 +1,5 @@
+https://github.com/woothemes/flexslider
+http://isotope.metafizzy.co/beta/
+https://github.com/desandro/imagesloaded
+http://jquery.eisbehr.de/lazy/
+http://beatak.github.io/jquery-imageloader/
